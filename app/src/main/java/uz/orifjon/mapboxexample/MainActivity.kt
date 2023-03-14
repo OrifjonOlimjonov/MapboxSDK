47bc9a074ff140cc309a25ebcc193f3b636a6964
Orifjon <orifjon.olimjonov@mail.ru>
2023-03-14 20:28:26 +0500

    configuration theme

--- app/src/main/java/uz/orifjon/mapboxexample/MainActivity.kt
+++ app/src/main/java/uz/orifjon/mapboxexample/MainActivity.kt
@@ -2,62 +2,30 @@ package uz.orifjon.mapboxexample
 
 import android.annotation.SuppressLint
 import android.app.PendingIntent
-import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
-import android.icu.util.MeasureUnit.POINT
+import android.content.res.Configuration
 import android.location.Location
 import android.os.Bundle
 import android.os.Looper
-import android.widget.Toast
-import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
-import androidx.core.content.ContextCompat
-import androidx.core.view.get
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.LifecycleOwner
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
 import com.mapbox.android.core.location.*
-import com.mapbox.android.gestures.MoveGestureDetector
-import com.mapbox.geojson.Point
 import com.mapbox.maps.*
 import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
-import com.mapbox.maps.extension.style.layers.properties.generated.IconAnchor
 import com.mapbox.maps.plugin.LocationPuck2D
-import com.mapbox.maps.plugin.animation.MapAnimationOptions
-import com.mapbox.maps.plugin.animation.camera
-import com.mapbox.maps.plugin.annotation.annotations
-import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
-import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
-import com.mapbox.maps.plugin.gestures.OnMoveListener
-import com.mapbox.maps.plugin.gestures.gestures
+import com.mapbox.maps.plugin.compass.compass
 import com.mapbox.maps.plugin.locationcomponent.*
-import com.mapbox.navigation.base.options.NavigationOptions
-import com.mapbox.navigation.core.MapboxNavigation
-import com.mapbox.navigation.core.lifecycle.MapboxNavigationApp
-import com.mapbox.navigation.core.lifecycle.MapboxNavigationObserver
-import com.mapbox.navigation.core.lifecycle.requireMapboxNavigation
-import com.mapbox.navigation.core.trip.session.LocationMatcherResult
-import com.mapbox.navigation.core.trip.session.LocationObserver
-import com.mapbox.navigation.ui.maps.location.NavigationLocationProvider
+import com.mapbox.maps.plugin.scalebar.scalebar
 import uz.orifjon.mapboxexample.databinding.ActivityMainBinding
 import uz.orifjon.mapboxexample.utils.LocationPermissionHelper
-import java.lang.Exception
 import java.lang.ref.WeakReference
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
 
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var locationPermissionHelper: LocationPermissionHelper
-
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,21 +33,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         locationPermissionHelper = LocationPermissionHelper(WeakReference(this))
         locationPermissionHelper.checkPermissions {
-            //binding.mapView.getMapboxMap().loadStyleUri(NavigationStyles.NAVIGATION_DAY_STYLE)
-//            if (!MapboxNavigationApp.isSetup()) {
-//                MapboxNavigationApp.setup {
-//                    NavigationOptions.Builder(this)
-//                        .accessToken("sk.eyJ1Ijoib3JpZmpvbm9saW1qb25vdiIsImEiOiJjbGY1NG9nNnUwN2g1NDFubjhzZDhyZDA3In0.cXmPKqrjEiaKUEOQB5rh0Q")
-//
-//                        // additional options
-//                        .build()
-//                }
-//            }
-
             onMapReady()
-
-
         }
+
 
 
     }
@@ -90,11 +46,28 @@ class MainActivity : AppCompatActivity() {
                 .zoom(14.0)
                 .build()
         )
-        binding.mapView.getMapboxMap().loadStyleUri(
-            Style.MAPBOX_STREETS
-        ) {
-            initLocationComponent()
+
+
+        binding.mapView.scalebar.enabled = false
+        binding.mapView.compass.enabled = false
+        when(resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES ->
+            {
+                binding.mapView.getMapboxMap().loadStyleUri(
+                    Style.DARK
+                ) {
+                    initLocationComponent()
+                }
+            }
+            Configuration.UI_MODE_NIGHT_NO->{
+                binding.mapView.getMapboxMap().loadStyleUri(
+                    Style.LIGHT
+                ) {
+                    initLocationComponent()
+                }
+            }
         }
+
     }
 
 
@@ -124,7 +97,6 @@ class MainActivity : AppCompatActivity() {
                     }
                 }.toJson()
             )
-
         }
     }
 
